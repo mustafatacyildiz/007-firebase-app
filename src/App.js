ae383521b19dba82310e4e9134f3e703fee4b5e2
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-04-12 12:18:57 +0300

    second submit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import AppRouter from "./router/AppRouter";
+
 
 const App = () => {
   return (
     <div>
-      <h2>Movie App</h2>
+      <AppRouter/>
     </div>
   );
 };
